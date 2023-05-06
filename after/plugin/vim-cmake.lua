693fc87a36e1526e58461bad23c971c6dd28bda0
hrzlgnm <hrzlgnm@users.noreply.github.com>
2023-05-06 19:48:43 +0200

    feat: bind some keys to vim-cmake plugin

--- after/plugin/vim-cmake.lua
+++ after/plugin/vim-cmake.lua
@@ -1,3 +1,6 @@
 local g = vim.g
 g.cmake_build_dir_location = 'build'
+vim.keymap.set('n', '<leader>cb', vim.cmd.CMakeBuild)
+vim.keymap.set('n', '<leader>cg', function() vim.cmd.CMakeGenerate('Release') end)
+vim.keymap.set('n', '<leader>cgd', function() vim.cmd.CMakeGenerate('Debug') end)
 
